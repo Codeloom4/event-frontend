9a0188b5035664e3931c95f60c918f6e55d0570f
EPIC\damith_r <damith_r@epiclanka.net>
2025-01-22 22:06:11 +0530

    Initial commit for event frontend project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-5xl font-bold text-center text-blue-600">
+      Event Plan System!
+    </h1>
+  );
+}
+
+export default App;
